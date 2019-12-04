81a4f28320185118349b284b0b5cd1fa395f8970
Fadjri Alfalah <25312375+fadjriaf@users.noreply.github.com>
2019-12-04 10:14:14 +0700

    Update SiswaExport.php
    fixing heading export with collection

--- app/Exports/SiswaExport.php
+++ app/Exports/SiswaExport.php
@@ -4,14 +4,27 @@ namespace App\Exports;
 
 use App\Siswa;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use DB;
 
-class SiswaExport implements FromCollection
+class SiswaExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Siswa::all();
+        // return Siswa::all();
+
+        $siswa = DB::table('siswa')->select('nama', 'nis', 'alamat')->get();
+        return $siswa ;
+    }
+
+    public function headings(): array {
+        return [
+            'Nama',
+            'Nis',
+            'Alamat'
+        ];
     }
 }
