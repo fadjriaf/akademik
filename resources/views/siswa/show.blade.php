8d6026a7384460872ac252c695ca8a11226baa8b
Fadjri Alfalah <25312375+fadjriaf@users.noreply.github.com>
2019-12-05 17:25:12 +0700

    Update show.blade.php
    added bootstrap template

--- resources/views/siswa/show.blade.php
+++ resources/views/siswa/show.blade.php
@@ -1,6 +1,26 @@
-<h1>Show {{ $siswa->nama }}</h1>
-<a href="/siswa"><< Back</a>
-<br>
-<p>Nama : {{ $siswa->nama }}</p>
-<p>NIS : {{ $siswa->nis }}</p>
-<p>Alamat : {{ $siswa->alamat }}</p>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tambah Siswa</title>
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+</head>
+<body>
+
+	<div class="container">
+
+		<div class="card mt-5">
+			<div class="card-header text-center">
+				Show Siswa {{ $siswa->nama }}
+			</div>
+			<div class="card-body">
+                <p>Nama : {{ $siswa->nama }}</p>
+                <p>NIS : {{ $siswa->nis }}</p>
+                <p>Alamat : {{ $siswa->alamat }}</p>
+			</div>
+            <div class="card-footer">
+            <a class="btn btn-primary" href="/siswa"><< Kembali</a> 
+            </div>
+		</div>
+	</div>
+</body>
+</html>
